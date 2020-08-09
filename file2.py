e6cd84b059bb3f8c25bc5311c03a2b09eea9249f
Helena Magaldi <helenamagaldi@gmail.com>
2020-08-09 18:10:51 -0300

    m

---
+++ file2.py
@@ -0,0 +1,11 @@
+# procedure in python 
+
+# lists may have different type of data, works better than array (yay) 
+lists = [1,2,3,'j']
+print(lists)
+
+# slicing
+lists[0:3]
+
+# accepts neg values 
+lists[-1]
