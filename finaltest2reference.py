af4a7099cf7e7a515f5d1484d19efd46ebff5aab
Helena Magaldi <helenamagaldi@gmail.com>
2020-10-08 12:51:01 -0300

    finishing program: acme

--- finaltest2reference.py
+++ finaltest2reference.py
@@ -1,7 +1,9 @@
-from datetime 
+from csv import DictReader
+from datetimedate,  
 import datetime 
 import csv
 import os
+from os import write
 
 def cadastrar_cliente():
     print('\n------ CADASTRO DE CLIENTES ------\n') cadastro = {}
@@ -61,4 +63,103 @@ def realizar_emprestimo():
     data_emprestimo = input("Data de empréstimo(dd/mm/aaaa)...: ")
 
     cadastro = {'nome_usuario': nome_usuario,'codigo_filme': codigo_filme, 'data_emprestimo': data_emprestimo}
-    
+    emprestimos_csv = open("emprestimos.csv", "a")
+    emprestimos_csv.write(f"{mp,e_usuario};{codigo_filme};{data_emprestimo}\n")
+    emprestimos_csv.close()
+
+#list = movies that have been borrowed
+def listar_emprestimos():
+    print('\n---- FILMES EMPRESTADOS ----\n')
+    emprestimos_csv = open('emprestimos.csv')
+    dados = csv.DicReader(emprestimos_csv, delimiter = ';')
+
+    print('f{emprestimo["nome_usuario"]:25}', f'{emprestimo["codigo_filme"]:20}', emprestimo["data_emprestimo"])
+    emprestimos_csv.close 
+
+#looking for CPF
+def pesquisar_cliente(nome_cliente):
+    clientes_csv = open('clientes.csv')
+    dados_clientes = csv.DictReader(clientes_csv, delimiter = ';')
+
+    for cliente in dados_clientes:
+        if cliente["nome"] == nome_cliente : 
+            cpf = cliente["cpf"]
+            return cpf
+            break
+            clientes_csv.close()
+
+# searching for movies name using its code
+def pesquisar_filme(codigo_filme):
+    filmes_csv = opem('filmes_csv')
+    dados_filmes = csv.DictReader(filmes_csv, delimiter = ';')
+
+    for filme in dados_filmes:
+        if filme['codigo'] == codigo_filme
+        nome_filme = filme['nome_filme']
+        return nome_filme
+        break
+        filmes_csv.close()
+
+#consulting data on loans and late reports
+def relatorio_atrasados():
+    emprestimos_csv = open('emprestimos.csv')
+    dados_emprestimos = csv.DictReader(emprestimos_csv, delimiter = ";")
+    relatorio = {}
+
+    print('\n\n-----  RELATÓRIO DE FILMES ATRASADOS NA DEVOLUÇÃO =====\n')
+    print(f'{"CPF":15}', f'{"NOME":15}', f'{"TITULO":25}', f'{"EMPRESTIMO";15}', f'{"SITUACAO":10}'', "DIAS")
+    for emprestimo in dados_emprestimos:
+        data_emprestimo = datetime.strptime(emprestimo["data_emprestimo"], '%d/%m/%Y')
+        data_joje = datetime.today()
+        dias = (data_hoje - data_emprestimo).days
+
+        if dias > 7:
+            situacao = 'Atrasado'
+            cpf = str(pesquisar_cliente(emprestimo["nome_usuario"]))
+            nome_filme = pesquisar_filme(emprestimo['codigo_filme'])
+
+            print(f'{cpf:15}'
+                f'{emprestimo["nome_usuario"]:15}',
+                f'{nome_filme:25}',
+                f'{emprestimo["data_emprestimo"]:15}',
+                f'{situacao:10}',
+                f'{str(dias-7):5'})
+
+#main program
+
+resposta = 's'
+while resposta == 's':
+    os.system('cls') or None
+
+    #main menu
+    menu = '''----- LOCADORA ACME -----\n
+    \r[1] Cadastrar Cliente
+    \r[2] Lista de Clientes
+    \r[3] Cadastrar Filme 
+    \r[4] Lista de Filmes
+    \r[5] Realizar Empréstimo
+    \r[6] Lista de Empréstimos
+    \r[7] Relatório de Atrasados
+    ''''
+    print(menu)
+    opcao = int(input('\n Escolha uma opção: '))
+    if opcao == 1:
+        cadastrar_cliente()
+    elif opcao == 2:
+        listar_clientes()
+    elif opcao == 3:
+        cadastrar_filme()
+    elif opcao == 4:
+        listar_filmes()
+    elif opcao == 5:
+        realizar_emprestimo()
+    elif opcao == 6:
+        listar_emprestimos()
+    elif opcao == 7:
+        relatorios_atrasados()
+    else:
+        print('\n Opção Inválida\n')
+
+    resposta = str(input('\n\n Deseja Continuar? [s/n]'))
+
+print('\n Obrigada \n')    
